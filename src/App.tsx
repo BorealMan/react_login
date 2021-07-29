4f39523ef63a135a2ceeb563ed1f27e056f52ecb
Ryan Kehrli <rkehrli1993@gmail.com>
2021-07-29 04:11:21 -0400

    default configuration

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import React, { } from 'react';
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Default App</h1>
+      <p>This is a default app configured to test project quickly, enjoy!</p>
+    </div>
+  );
+}
+
+export default App;
